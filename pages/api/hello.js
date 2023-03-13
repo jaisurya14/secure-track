620e86e08bc797ac93c48d2b816094f670344ab6
MaarcusRenieroL <maarcusreniero@gmail.com>
2023-03-13 10:40:04 +0530

    api updated

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,4 +1,5 @@
-export default async (req, res) => {
-    res.statusCode = 200
-    res.json({ name: 'John Doe' })
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({ name: 'John Doe' })
 }
